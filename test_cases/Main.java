dbd5745ed526d88546af330100ff0562f627d0d3
Ole Kristian Aune <okaune@outlook.com>
2015-10-28 14:52:31 +0100

    Siste versjon av main

--- test_cases/Main.java
+++ test_cases/Main.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 import lejos.hardware.motor.*; // Importing all motor classes
 import lejos.hardware.lcd.*; // Importing all LCD screen classes
 import lejos.hardware.*; // Importing all hardware classes
@@ -11,68 +13,71 @@ import lejos.robotics.navigation.DifferentialPilot; // Importing DifferentialPil
 public class Main{
 	public static void main (String[] arg) throws Exception  {
 
-		driveTest();
+		// colorTest(); FUNGERER IKKE!!! LAG NY!
+
+		ultrasonicTest();
 
 	}
 
-	public static void driveTest() {
+	public static void driveTest() throws Exception {
 		/* This test drives the robot left and then right (back to the starting position) */
 
-		Motor driveMotor = Motor.A; // Check correct motor
+		// Motor.C
 
-		driveMotor.setSpeed(200); // Value for motorspeeed?
-		driveMotor.forward(); // Drive one direction
-		Thread.sleep(100);
-		driveMotor.backward(); // Drive other direction
-		Thread.sleep(100);
+		Motor.C.setSpeed(200); // Value for motorspeeed?
+		Motor.C.forward(); // Drive one direction
+		Thread.sleep(1000);
+		Motor.C.backward(); // Drive other direction
+		Thread.sleep(1000);
 	}
 
-	public static void moveArmTest() {
+	public static void moveArmTest(int value) {
 		/* This test moves the "arm" up and then down again */
 
-		Motor armMotor = Motor.B // Check correct motor
+		// Motor.B
 
-		armMotor.setSpeed(50); // Value for motorspeeed?
-		armMotor.rotateTo(360); // Value for rotation at up-position?
-		armMotor.rotateTo(180); // Value for rotation at down-position?
+		Motor.B.setSpeed(200); // Value for motorspeeed?
+		Motor.B.rotateTo(value); // Top: rotateTo(-90)
 	}
 
-	public static void moveLeverTest() {
+	public static void moveLeverTest(int value) {
 		/* This test moved the lever up and down again */
 
-		Motor armMotor = Motor.C // Check correct motor
+		// Motor.D
 
-		armMotor.setSpeed(50); // Value for motorspeeed?
-		armMotor.rotateTo(360); // Value for rotation at up-position?
-		armMotor.rotateTo(180); // Value for rotation at down-position?
+		Motor.D.setSpeed(200); // Value for motorspeeed?
+		Motor.D.rotateTo(value); // Top: rotateTo(70)
 	}
 
 	public static void ultrasonicTest() {
 		/* This test logs the values of the ultrasonic sensor */
 
 		Brick brick = BrickFinder.getDefault(); // Register brick
-		Port s1 = brick.getPort("S1"); // Register EV3-uttrasonicsensor port
-		EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(s1); // Register EV3-uttrasonicsensor
+		Port s3 = brick.getPort("S3"); // Register EV3-uttrasonicsensor port
+		EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(s3); // Register EV3-uttrasonicsensor
 		SampleProvider ultrasonicLeser = ultrasonicSensor.getDistanceMode(); // Register sample provider for EV3-uttrasonicsensor
 		float[] ultrasonicSample = new float[ultrasonicLeser.sampleSize()]; // Register float table for EV3-uttrasonicsensor sample values
 
 		String filename = "ultrasonicOutput.txt"; // Filename of output file
 		File file = new File(filename); // Register as file
+		String output = "";
 
 		for (int i = 0; i < 100; i++) {
 			ultrasonicLeser.fetchSample(ultrasonicSample, 0); // Save values to first position of the EV3-uttrasonicsensor float table
 
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-				// Create new file if no file exsist
-            	if ( !file.exists() ) {
-                	file.createNewFile();
-            	}
+			output += ultrasonicSample[0] + "\n";
+		}
 
-                writer.write(Float.toString(ultrasonicSample[0])); // Writes value to file
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+			// Create new file if no file exsist
+			if ( !file.exists() ) {
+				file.createNewFile();
+			}
 
-        	} catch (IOException e) {
-            	System.out.println(e.getMessage());
-        	}
+			writer.write(output); // Writes value to file
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
 		}
 
 	}
@@ -81,28 +86,30 @@ public class Main{
 		/* This test logs the values of the color sensor */
 
 		Brick brick = BrickFinder.getDefault(); // Register brick
-		Port s2 = brick.getPort("S2"); // Register EV3-uttrasonicsensor port
-		EV3ColorSensor colorSensor = new EV3ColorSensor(s2); // Register EV3-uttrasonicsensor
+		Port s4 = brick.getPort("S4"); // Register EV3-uttrasonicsensor port
+		EV3ColorSensor colorSensor = new EV3ColorSensor(s4); // Register EV3-uttrasonicsensor
 		SampleProvider colorLeser = colorSensor; // Register sample provider for EV3-uttrasonicsensor
 		float[] colorSample = new float[colorLeser.sampleSize()]; // Register float table for EV3-uttrasonicsensor sample values
 
 		String filename = "colorOutput.txt"; // Filename of output file
 		File file = new File(filename); // Register as file
+		String output = "";
 
 		for (int i = 0; i < 100; i++) {
 			colorLeser.fetchSample(colorSample, 0); // Save values to first position of the EV3-uttrasonicsensor float table
+			output += colorSample[0] + "\n";
+		}
 
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-				// Create new file if no file exsist
-            	if ( !file.exists() ) {
-                	file.createNewFile();
-            	}
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+			// Create new file if no file exsist
+			if ( !file.exists() ) {
+				file.createNewFile();
+			}
 
-                writer.write(Float.toString(colorSample[0])); // Writes value to file
+			writer.write(output); // Writes value to file
 
-        	} catch (IOException e) {
-            	System.out.println(e.getMessage());
-        	}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
