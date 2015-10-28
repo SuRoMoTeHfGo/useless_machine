283faacde7df9281eef2c235ebfbbf5b16860ae3
Ole Kristian Aune <okaune@outlook.com>
2015-10-28 12:57:20 +0100

    Lagt opp filer til utvikling av tester

---
+++ test_cases/Main.java
@@ -0,0 +1,17 @@
+import lejos.hardware.motor.*; // Importing all motor classes
+import lejos.hardware.lcd.*; // Importing all LCD screen classes
+import lejos.hardware.*; // Importing all hardware classes
+import lejos.hardware.port.Port; // Importing Port class
+import lejos.hardware.ev3.EV3; // Importing EV3 class
+import lejos.robotics.SampleProvider; // Importing SampleProvider class
+import lejos.hardware.sensor.*; // Importing all sensor classes
+import lejos.robotics.navigation.DifferentialPilot; // Importing DifferentialPilot class
+
+
+public class Main{
+	public static void main (String[] arg) throws Exception  {
+
+		
+
+	}
+}
