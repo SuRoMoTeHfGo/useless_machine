d057593bca282081c5bcf97dd796159b162fab85
Elias <eliassorr@gmail.com>
2015-11-17 13:22:33 +0100

    Lagt ved lydfil og implementerte klasse AudioPlayer

--- Audioplayer.java
+++ Audioplayer.java
@@ -8,13 +8,45 @@
 import lejos.hardware.Brick;
 import lejos.hardware.port.Port;
 import lejos.hardware.BrickFinder;
-import lejos.hardware.ev3.EV3;
+import lejos.hardware.ev3.*;
 import lejos.hardware.Keys;
 import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
-import lejos.hardware.sensor.*;
+import lejos.robotics.pathfinding.Path;
+import lejos.hardware.Sounds.*;
+import lejos.hardware.Audio.*;
+import lejos.remote.ev3.*;
+;import lejos.hardware.Sound;
 
+//Java classes
+import java.io.File;
 public class Audioplayer{
+// Object variables
+	private int masterVolume;
+	File file;
+// Constructor
+	public Audioplayer(int masterVolume){
+		this.masterVolume = masterVolume;
+		file = new File("MegaMan.wav");
+	}
+	public int generateSound(File sound)throws Exception{
+		return Sound.playSample(sound,50);
+	}
+	public void setVolume()throws Exception{
+		Sound.setVolume(100);
+	}
+	private void getSound()throws Exception{
+		Runnable task = new Runnable() {//en tråd som spiller av sangen/musikken. vi kan legge til spillelister senere..
+		public void run() {
+			try{
+				int wavfilelength = generateSound(file);
+				} catch (Exception e){
+
+					}
+				}
+			};
+			new Thread(task).start();
+	}
 
 }
 
